d4470f6b3689714d1924c9c68e547292e5f044b6
Felix Kaechele <felix@kaechele.ca>
2022-04-08 22:28:03 -0400

    Use upper device of batman-adv device for node_id and mac
    Replicate the behaviour batman-adv exhibits when bound to an upper
    device.
    
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- providers/nodeinfo/network/mac.py
+++ providers/nodeinfo/network/mac.py
@@ -1,8 +1,16 @@
 import providers
+from glob import glob
+
 
 class Source(providers.DataSource):
     def required_args(self):
         return ['batadv_dev']
 
     def call(self, batadv_dev):
-        return open('/sys/class/net/' + batadv_dev + '/address').read().strip()
+        upper = glob('/sys/class/net/' + batadv_dev + '/upper*')
+        if len(upper) > 0:
+            sysfs_path = upper[0]
+        else:
+            sysfs_path = '/sys/class/net/' + batadv_dev
+
+        return open(sysfs_path + '/address').read().strip()
